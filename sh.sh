094e288de34e2e16d54a75447b05ab535bf8e43a
Ubuntu <ubuntu@ip-172-31-32-196.ap-south-1.compute.internal>
2023-12-02 11:59:01 +0000

    done

--- sh.sh
+++ sh.sh
@@ -5,7 +5,7 @@ CID=$FILE
 filelist=$(git diff-tree --no-commit-id --name-only -r $CID )
 
 echo $filelist
-if [[ $filelist == *"File2"* ]]; then
+if [[ $filelist == *"file2"* ]]; then
         # call the URL according to your build trigger config
     touch /home/ubuntu/jenkins_particular_file
 
